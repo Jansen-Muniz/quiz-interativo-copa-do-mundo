3cf24a5ce109f8b4ff515fb5623d449f236802ba
Jansen Muniz <jansen.muniz1@gmail.com>
2022-10-27 18:19:54 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const form = document.querySelector('.quiz-form')
+const finalScoreContainer = document.querySelector('.final-score-container')
+
+const correctAnswers = ['C', 'C', 'B', 'B']
+
+let score = 0
+
+const getUserAnswers = () => correctAnswers.map((_, index) =>
+  form[`inputQuestion${index + 1}`].value)
+
+const calculateUserScore = userAnswers => {
+  userAnswers.forEach((userAnswer, index) => {
+    const isUserAnswerCorrect = userAnswer === correctAnswers[index]
+
+    if (isUserAnswerCorrect) {
+      score += 25
+    }
+  })
+}
+
+const showFinalScore = () => {
+  scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  })
+  finalScoreContainer.classList.remove('d-none')
+}
+
+const animateFinalScore = () => {
+  let counter = 0
+
+  const timer = setInterval(() => {
+    if (counter === score) {
+      clearInterval(timer)
+    }
+
+    finalScoreContainer.querySelector('span').textContent = `${counter++}%`
+  }, 10)
+}
+
+form.addEventListener('submit', event => {
+  event.preventDefault()
+
+  const userAnswers = getUserAnswers()
+
+  calculateUserScore(userAnswers)
+  showFinalScore()
+  animateFinalScore()
+})
